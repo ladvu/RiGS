d1438fb4e2d2c5da0c245642066a7022fc0c25fe
Chenyu <chenyu@Chenyus-MacBook-Pro.local>
2026-05-17 21:11:22 +0800

    add pages

--- dependencies/RAFT/download_models.py
+++
@@ -1,5 +0,0 @@
-import gdown
-import os
-gdown.download_folder("https://drive.google.com/drive/folders/1YLovlvUW94vciWvTyLf-p3uWscbOQRWW")
-
-os.rename('SEA-RAFT-models', 'models')
--- src/runner/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dynamic import DynamicRunner
-from .static import StaticRunner
-# from .fused import FusedRunner
-from .fused_v2 import FusedRunner
