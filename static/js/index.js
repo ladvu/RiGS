2eb8077b75d3062e1c0fa074af69f93f0118001e
Chenyu <chenyu@Chenyus-MacBook-Pro.local>
2026-06-05 03:32:55 -0600

    update page

--- static/js/index.js
+++ static/js/index.js
@@ -1,78 +1,94 @@
-window.HELP_IMPROVE_VIDEOJS = false;
-
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
-
-var interp_images = [];
-function preloadInterpolationImages() {
-  for (var i = 0; i < NUM_INTERP_FRAMES; i++) {
-    var path = INTERP_BASE + '/' + String(i).padStart(6, '0') + '.jpg';
-    interp_images[i] = new Image();
-    interp_images[i].src = path;
-  }
-}
-
-function setInterpolationImage(i) {
-  var image = interp_images[i];
-  image.ondragstart = function() { return false; };
-  image.oncontextmenu = function() { return false; };
-  $('#interpolation-image-wrapper').empty().append(image);
-}
-
-
 $(document).ready(function() {
-    // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
+  $(".navbar-burger").click(function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
 
+  if (document.querySelector('.carousel')) {
+    bulmaCarousel.attach('.carousel', {
+      slidesToScroll: 1,
+      slidesToShow: 3,
+      loop: true,
+      infinite: true,
+      autoplay: false,
+      autoplaySpeed: 3000,
     });
+  }
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 3,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
-    }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
-
-    // Access to bulmaCarousel instance of an element
-    var element = document.querySelector('#my-element');
-    if (element && element.bulmaCarousel) {
-    	// bulmaCarousel instance is available as element.bulmaCarousel
-    	element.bulmaCarousel.on('before-show', function(state) {
-    		console.log(state);
-    	});
-    }
-
-    /*var player = document.getElementById('interpolation-video');
-    player.addEventListener('loadedmetadata', function() {
-      $('#interpolation-slider').on('input', function(event) {
-        console.log(this.value, player.duration);
-        player.currentTime = player.duration / 100 * this.value;
-      })
-    }, false);*/
-    preloadInterpolationImages();
-
-    $('#interpolation-slider').on('input', function(event) {
-      setInterpolationImage(this.value);
-    });
-    setInterpolationImage(0);
-    $('#interpolation-slider').prop('max', NUM_INTERP_FRAMES - 1);
-
+  if (window.bulmaSlider) {
     bulmaSlider.attach();
+  }
 
-})
+  $(".multi-compare").each(function() {
+    var compare = this;
+    var ranges = Array.from(compare.querySelectorAll(".multi-range"));
+    var minGap = 6;
+    var activeIndex = 1;
+
+    function clampHandles(activeRange) {
+      var values = ranges.map(function(range) {
+        return Number(range.value);
+      });
+      var index = ranges.indexOf(activeRange);
+
+      if (index === 0) {
+        values[0] = Math.min(values[0], values[1] - minGap);
+      } else if (index === 1) {
+        values[1] = Math.max(values[0] + minGap, Math.min(values[1], values[2] - minGap));
+      } else if (index === 2) {
+        values[2] = Math.max(values[2], values[1] + minGap);
+      }
+
+      values[0] = Math.max(5, Math.min(values[0], 95 - minGap * 2));
+      values[1] = Math.max(values[0] + minGap, Math.min(values[1], 95 - minGap));
+      values[2] = Math.max(values[1] + minGap, Math.min(values[2], 95));
+
+      ranges.forEach(function(range, i) {
+        range.value = values[i];
+        compare.style.setProperty("--p" + (i + 1), values[i] + "%");
+      });
+    }
+
+    function setHandleFromPointer(event) {
+      var rect = compare.getBoundingClientRect();
+      var percent = Math.max(5, Math.min(95, (event.clientX - rect.left) / rect.width * 100));
+      ranges[activeIndex].value = percent;
+      clampHandles(ranges[activeIndex]);
+    }
+
+    ranges.forEach(function(range) {
+      range.addEventListener("input", function() {
+        clampHandles(range);
+      });
+    });
+
+    compare.addEventListener("pointerdown", function(event) {
+      var rect = compare.getBoundingClientRect();
+      var percent = Math.max(5, Math.min(95, (event.clientX - rect.left) / rect.width * 100));
+      var values = ranges.map(function(range) {
+        return Number(range.value);
+      });
+
+      activeIndex = values.reduce(function(bestIndex, value, index) {
+        return Math.abs(value - percent) < Math.abs(values[bestIndex] - percent) ? index : bestIndex;
+      }, 0);
+
+      compare.setPointerCapture(event.pointerId);
+      setHandleFromPointer(event);
+    });
+
+    compare.addEventListener("pointermove", function(event) {
+      if (compare.hasPointerCapture(event.pointerId)) {
+        setHandleFromPointer(event);
+      }
+    });
+
+    compare.addEventListener("pointerup", function(event) {
+      if (compare.hasPointerCapture(event.pointerId)) {
+        compare.releasePointerCapture(event.pointerId);
+      }
+    });
+
+    clampHandles(ranges[1]);
+  });
+});
